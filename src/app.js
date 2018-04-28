cc87ad06d86ce9ae4fa0abd1c57f42bb66ffcab0
David MacDonald <davidjohnmacdonald@gmail.com>
2018-04-28 12:46:20 -0400

    Moves offline JSON to assets.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 // currently using my dev copy of the sheet which has the urls in a separate column
-const sheet = 'https://sheets.googleapis.com/v4/spreadsheets/191ExX5H64wdAvxqfdaxj13bHn31hDifH8uhczhzkyLU/values/Women%20Composers%20Database!A3:AK?key=AIzaSyA-h6VkeSPqfe299CwSS88O-qwI2MVQw0A';
+//const sheet = 'https://sheets.googleapis.com/v4/spreadsheets/191ExX5H64wdAvxqfdaxj13bHn31hDifH8uhczhzkyLU/values/Women%20Composers%20Database!A3:AK?key=AIzaSyA-h6VkeSPqfe299CwSS88O-qwI2MVQw0A';
+
+// ofline version
+const sheet = 'assets/wcdb-offline.json';
+
 const {flag, name, code} = require('country-emoji');
 
 const fields = [
