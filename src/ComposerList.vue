1fb3dac149d23835021abce3b19e863f735724ae
ianring <heyianring@hotmail.com>
2018-06-10 04:27:53 -0400

    added Vue I18n, and en translation of static strings.

--- src/ComposerList.vue
+++ src/ComposerList.vue
@@ -2,15 +2,15 @@
     <div class="list-wrapper">
         <ul class="composer-list">
             <template v-for="composer in filteredList">
-                    <li>
-                        <span class="name"><a :href="composer[composer.length-1]" target="_blank">{{composer[0]}}</a></span>
-                        <span class="composer-props">
-                            <template v-for="(field, i) in composer" v-if="(field=='X' && cardBadges.indexOf(vueFields[i].type) > -1)">
-                                <span class="badge" :class="[vueFields[i].type, vueFields[i].class, { selected : filters[i] }]" @click="toggleFilter(i)">{{vueFields[i].icon}}</span>
-                            </template>
-                        </span>
-                        <span class="composer-geo" v-html="composerGeo(composer)"></span>
-                    </li>
+                <li>
+                    <span class="name"><a :href="composer[composer.length-1]" target="_blank">{{composer[0]}}</a></span>
+                    <span class="composer-props">
+                        <template v-for="(field, i) in composer" v-if="(field=='X' && cardBadges.indexOf(vueFields[i].type) > -1)">
+                            <span class="badge" :class="[vueFields[i].type, vueFields[i].class, { selected : filters[i] }]" @click="toggleFilter(i)">{{vueFields[i].icon}}</span>
+                        </template>
+                    </span>
+                    <span class="composer-geo" v-html="composerGeo(composer)"></span>
+                </li>
             </template>
         </ul>
     </div>
@@ -34,45 +34,50 @@ export default {
                 return '';
             }
 
-    			var flagmoji = flag( country );
+			var flagmoji = flag( country );
 
-    			if (flagmoji == null && country.indexOf('\/') > -1){
-    				flagmoji = '';
-    				var countries = country.split('\/');
+			if (flagmoji == null && country.indexOf('\/') > -1){
+				flagmoji = '';
+				var countries = country.split('\/');
 
-    				for (var i = 0; i < countries.length; i++){
-    					flagmoji += flag( countries[i] );
-    				}
-    			}
+				for (var i = 0; i < countries.length; i++){
+					flagmoji += flag( countries[i] );
+				}
+			}
 
-			       return flagmoji;
-		      }, // getFlags
+			return flagmoji;
+		},
+        
         composerGeo: function(row) { // return span for geographical information for each composer
-      			// var cityField = this.startOfSection('geographic');
-      			var cityField = 40;
-      			var countryField = cityField + 2;
-      			var geoSpan = '';
-      			var nodata = ['N/A', '']
-      			if (!nodata.includes(row[cityField])){ // if there's a city/state, give that
-      				geoSpan = row[cityField];
+  			// var cityField = this.startOfSection('geographic');
+  			var cityField = 40;
+  			var countryField = cityField + 2;
+  			var geoSpan = '';
+  			var nodata = ['N/A', '']
+  			if (!nodata.includes(row[cityField])){ // if there's a city/state, give that
+  				geoSpan = row[cityField];
 
-      				if (!nodata.includes(row[countryField])) { // if there's *also* a country, add that
-      					geoSpan += ', ' + row[countryField] + ( this.getFlags( row[countryField] ) ? (' ' + this.getFlags( row[countryField] )) : '' );
-      				}
-      			}
+  				if (!nodata.includes(row[countryField])) { // if there's *also* a country, add that
+  					geoSpan += ', ' + row[countryField] + ( this.getFlags( row[countryField] ) ? (' ' + this.getFlags( row[countryField] )) : '' );
+  				}
+  			}
 
-      			if (geoSpan == '' && !nodata.includes(row[countryField])) { // if there's only a country, give that
-      				geoSpan = row[countryField] + ( this.getFlags( row[countryField] ) ? (' ' + this.getFlags( row[countryField] )) : '' );
-      			}
-      			return geoSpan;
-      		}, // composerGeo
+  			if (geoSpan == '' && !nodata.includes(row[countryField])) { // if there's only a country, give that
+  				geoSpan = row[countryField] + ( this.getFlags( row[countryField] ) ? (' ' + this.getFlags( row[countryField] )) : '' );
+  			}
+  			return geoSpan;
+  		},
+
         toggleFilter: function(filter) {
             this.$emit('toggleFilter', {filter})
-        }, // toggleFilter
+        },
+
         updateTotal: function() {
             this.filteredTotal = this.filteredList.length;
         }
-    }, // methods
+
+    },
+
     // beforeUpdate: {
     //
     // }
